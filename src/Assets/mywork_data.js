e806d69f5af9b9162e61ba7c049a698dcdab5604
kapilpoonia22 <pooniakapil59@gmail.com>
2025-07-11 09:48:32 +0530

    Updated About section for fresher look with CSS changes

--- src/Assets/mywork_data.js
+++ src/Assets/mywork_data.js
@@ -1,41 +1,53 @@
-import pic1 from './project_1.svg';
-import pic2 from './project_2.svg';
-import pic3 from './project_3.svg';
+import pic1 from './tic-tac-toc.png';
+import pic2 from './booking.png';
+import pic3 from './portfolio.png';
 import pic4 from './project_4.svg';
 import pic5 from './project_5.svg';
 import pic6 from './project_6.svg';
 
 const mywork_data = [
-    {
-        w_no:1,
-        w_name:"Web design",
-        w_img:pic1
-    },
-    {
-        w_no:2,
-        w_name:"Web design",
-        w_img:pic2
-    },
-    {
-        w_no:3,
-        w_name:"Web design",
-        w_img:pic3
-    },
-    {
-        w_no:4,
-        w_name:"Web design",
-        w_img:pic4
-    },
-    {
-        w_no:5,
-        w_name:"Web design",
-        w_img:pic5
-    },
-    {
-        w_no:6,
-        w_name:"Web design",
-        w_img:pic6
-    },
-]
- 
+  {
+    w_no: 1,
+    w_name: "Web Design",
+    w_img: pic1,
+    git_url: "https://github.com/kapilpoonia22",
+    live_url: "https://tic-toc-two.vercel.app/"
+  },
+  {
+    w_no: 2,
+    w_name: "Web Design",
+    w_img: pic2,
+    git_url: "https://github.com/kapilpoonia22",
+    live_url: "https://hospital-booking-lemon.vercel.app/"
+  },
+  {
+    w_no: 3,
+    w_name: "Web Design",
+    w_img: pic3,
+    git_url: "https://github.com/kapilpoonia22",
+    live_url: "https://yourproject3.live"
+  },
+  {
+    w_no: 4,
+    w_name: "Web Design",
+    w_img: pic4,
+    git_url: "https://github.com/youruser/project4",
+    live_url: "https://portfoilo-nine-iota.vercel.app/"
+  },
+  {
+    w_no: 5,
+    w_name: "Web Design",
+    w_img: pic5,
+    git_url: "https://github.com/youruser/project5",
+    live_url: "https://yourproject5.live"
+  },
+  {
+    w_no: 6,
+    w_name: "Web Design",
+    w_img: pic6,
+    git_url: "https://github.com/youruser/project6",
+    live_url: "https://yourproject6.live"
+  },
+];
+
 export default mywork_data;
--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -4,24 +4,36 @@ import theme_pattern from '../../Assets/theme_pattern.svg';
 import mywork_data from './../../Assets/mywork_data';
 import arrow_icon from '../../Assets/arrow_icon.svg';
 
-
 const MyWork = () => {
   return (
     <div id='work' className='mywork'>
-        <div className='mywork-title'>
+      <div className='mywork-title'>
         <h1>My latest work</h1>
         <img src={theme_pattern} alt='' />
-        </div>
-        <div className='mywork-container'>
-            {mywork_data.map((work,index)=>{
-                return <img key={index} src={work.w_img} alt='' />
-            })}
-        </div>
-        <div className='mywork-showmore'>
-          <p>Show More</p>
-          <img src={arrow_icon} alt=''/>
-        </div>
+      </div>
+
+      <div className='mywork-container'>
+        {mywork_data.map((work, index) => (
+          <div className="mywork-card" key={index}>
+            <img src={work.w_img} alt={work.w_name} />
+            <div className="overlay">
+              <a href={work.git_url} target="_blank" rel="noopener noreferrer">
+                <button>GitHub</button>
+              </a>
+              <a href={work.live_url} target="_blank" rel="noopener noreferrer">
+                <button>View Project</button>
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className='mywork-showmore'>
+        <p>Show More</p>
+        <img src={arrow_icon} alt='' />
+      </div>
     </div>
-  )
-}
-export default MyWork
+  );
+};
+
+export default MyWork;
